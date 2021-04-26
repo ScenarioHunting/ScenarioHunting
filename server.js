5d4fb92cb6df2b631425cd482d511a8e33616312
Mohsen-Bazmi <Mohsen.Bazmi@gmail.com>
2021-04-26 23:04:29 +0430

    refactor: Add ScenarioBuilderService class

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ const path = require('path')
 const port = process.env.PORT || 3001
 
 app.use('/', express.static(path.join(__dirname, 'dist')))
-// app.use('/default-templates', express.static(path.join(__dirname, 'dist', 'default-templates')))
 
 app.listen(port, () => console.log("Listening on Port", port)) 
