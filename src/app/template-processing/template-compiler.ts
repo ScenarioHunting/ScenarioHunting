7120742c2fa9a446a2b3d2038c1217d99169c2a6
Mohsen-Bazmi <Mohsen.Bazmi@gmail.com>
2021-04-30 17:31:01 +0430

    refactor: Parse the handlebar's compile errors

--- src/app/template-processing/template-compiler.ts
+++ src/app/template-processing/template-compiler.ts
@@ -32,10 +32,23 @@ Object.entries(helpers).map(([name, fn]) => Handlebars.registerHelper(name, fn))
 
 export class TemplateCompiler {
     compileTemplate(template: string, testSchema): string {
+        try {
+            var compiledTemplate = Handlebars.compile(template);
 
-        var compiledTemplate = Handlebars.compile(template);
-
-        //TODO: validate: the test must have When and Then at least.
-        return compiledTemplate(testSchema)
+            //TODO: validate: the test must have When and Then at least.
+            return compiledTemplate(testSchema)
+        } catch (err) {
+            let wrappedError = {
+                hasLineNumber: false,
+                lineNumber: 0,
+                internalError: err
+            }
+            const lineNumberMatch = String(err).match(/error on line (\d+)/)
+            if (lineNumberMatch && lineNumberMatch.length > 1) {
+                wrappedError.lineNumber = parseInt(lineNumberMatch[1])
+                wrappedError.hasLineNumber = true
+            }
+            throw wrappedError
+        }
     }
 }
