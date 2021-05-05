a569677ff17e4ccf2b2cc797b5ef0322c59d194f
Mohsen-Bazmi <Mohsen.Bazmi@gmail.com>
2021-05-06 00:21:01 +0430

    fix: Set the editor's external services path in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,137 +3,141 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 // const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 const externalServicesPath = './src/external-services.tsx'
-const appConfig = {
-	name: 'app',
-	// optimization: {
-	// 	minimize: true,
-	// 	emitOnErrors: true,
-	// 	mergeDuplicateChunks: true,
-	// 	removeAvailableModules: true,
-	// 	removeEmptyChunks: true,
-	// 	flagIncludedChunks: true,
-	// 	// runtimeChunk: 'single',
-	// },
-	// mode: 'production',
-	mode: 'development',
+function appConfig() {
+	return {
+		name: 'app',
+		optimization: {
+			minimize: true,
+			emitOnErrors: true,
+			mergeDuplicateChunks: true,
+			removeAvailableModules: true,
+			removeEmptyChunks: true,
+			flagIncludedChunks: true,
+			// runtimeChunk: 'single',
+		},
+		mode: 'production',
+		// mode: 'development',
 
-	entry: {
-		index: './src/index.ts',
-		app: {
-			import: './src/app/app.tsx',
+		entry: {
+			index: './src/index.ts',
+			app: {
+				import: './src/app/app.tsx',
+				//To split this file in order to be able to access it by the template editor:
+				dependOn: ['ExternalServices']
+
+			},
 			//To split this file in order to be able to access it by the template editor:
-			dependOn: ['ExternalServices']
+			ExternalServices: externalServicesPath
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|js)x?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				},
+				{
+					test: /\.css$/,
+					use: [{
+						loader: "style-loader"
+					},
+					{ loader: "css-modules-typescript-loader" },
+					{
+						loader: "css-loader",
+
+						options: {
+							importLoaders: 1,
+							modules: {
+								localIdentName: '[name]__[local]___[hash:base64:5]'
+							}
+						}
+					},
+						// {
+						// 	loader: "less-loader"
+						// }
+					],
+					exclude: /node_modules/
+				},
+				{
+					test: /\.(png|svg|jpg|gif)$/,
+					use: [
+						'file-loader',
+					]
+				},
+				{
+					test: '/src/template-processing/monaco-editor.js',
+					use: [
+						'file-loader',
+					]
+				},
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', 'css', '.js', '.svg']
+		},
+		output: {
+			filename: '[name].js',
+			// chunkFilename: '[id].chunk.js',
+			path: path.resolve(__dirname, 'dist'),
+			// clean: true,
+			libraryTarget: 'var',
+			library: 'ExternalServices'
+		},
+		plugins: [
+			new HtmlWebPackPlugin({
+				template: "./src/index.html",
+				filename: "./index.html",
+				excludeChunks: ['ExternalServices'],
+			}),
+			new HtmlWebPackPlugin({
+				template: "./src/app/app.html",
+				filename: "./app.html",
+				excludeChunks: ['index']
+			}),
+		]
+	}
+}
+function editorConfig() {
+	return {
+		name: 'editor',
+		// mode: 'production',
+		mode: 'development',
+		entry: {
+			editor: "./src/app/template-processing/editor.js",
+			monacoLanguage: './src/app/template-processing/monaco-languages.js',
+			ExternalServices: externalServicesPath
 
 		},
-		//To split this file in order to be able to access it by the template editor:
-		ExternalServices: externalServicesPath
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [{
-					loader: "style-loader"
-				},
-				{ loader: "css-modules-typescript-loader" },
+		module: {
+			rules: [
 				{
-					loader: "css-loader",
-
-					options: {
-						importLoaders: 1,
-						modules: {
-							localIdentName: '[name]__[local]___[hash:base64:5]'
-						}
-					}
+					test: /\.(ts|js)x?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
 				},
-					// {
-					// 	loader: "less-loader"
-					// }
-				],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader',
-				]
-			},
-			{
-				test: '/src/template-processing/monaco-editor.js',
-				use: [
-					'file-loader',
-				]
-			},
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', 'css', '.js', '.svg']
-	},
-	output: {
-		filename: '[name].js',
-		// chunkFilename: '[id].chunk.js',
-		path: path.resolve(__dirname, 'dist'),
-		// clean: true,
-		libraryTarget: 'var',
-		library: 'ExternalServices'
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: "./src/index.html",
-			filename: "./index.html",
-			excludeChunks: ['ExternalServices'],
-		}),
-		new HtmlWebPackPlugin({
-			template: "./src/app/app.html",
-			filename: "./app.html",
-			excludeChunks: ['index']
-		}),
-	]
-}
-const editorConfig = {
-	name: 'editor',
-	// mode: 'production',
-	mode: 'development',
-	entry: {
-		editor: "./src/app/template-processing/editor.js",
-		monacoLanguage: './src/app/template-processing/monaco-languages.js',
-		// ExternalServices: externalServicesPath
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', 'css', '.js', '.svg']
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			chunkFilename: '[id].chunk.js',
+			libraryTarget: 'var',
+			library: 'ExternalServices'
 
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			},
+		},
+		plugins: [
+			new HtmlWebPackPlugin({
+				template: "./src/app/template-processing/monaco-editor.html",
+				filename: "./monaco-editor.html",
+				// chucks: ['ExternalServices'],
+				inject: 'body',
+				scriptLoading: 'blocking'
+			}),
+			// new MonacoWebpackPlugin()
 		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', 'css', '.js', '.svg']
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		chunkFilename: '[id].chunk.js',
-		libraryTarget: 'var',
-		library: 'ExternalServices'
-
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: "./src/app/template-processing/monaco-editor.html",
-			filename: "./monaco-editor.html",
-			// chucks: ['ExternalServices'],
-			inject: 'body',
-			scriptLoading: 'blocking'
-		}),
-		// new MonacoWebpackPlugin()
-	]
+	}
 }
-module.exports = [appConfig, editorConfig]
+module.exports = (env) => [appConfig(), editorConfig()]
 module.exports.parallelism = 1;
