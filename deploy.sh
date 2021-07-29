b5d5bfc070c9f902cb8578e59553c80fa9e0d493
Mohsen-Bazmi <Mohsen.Bazmi@gmail.com>
2021-07-29 23:37:08 +0430

    chore:Setup miro dependencies

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ git commit -m 'chore:Setup miro dependencies'
 npm run build 
 npx gh-pages -d dist --repo https://github.com/ScenarioHunting/ScenarioHunting 
 sed -i 's|ExternalServices = createMiroDependencies|ExternalServices = createMockedDependencies|g' ./src/external-services.tsx 
-git add . 
-git commit -m 'chore: setup development dependencies' 
+git revert HEAD~1..HEAD
 git checkout develop 
 git merge release 
 git branch -d release
