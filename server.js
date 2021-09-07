b4d5dc1152c2c94065fe9c85d19e9d40ef4108c8
Mohsen-Bazmi <Mohsen.Bazmi@gmail.com>
2021-09-07 22:42:27 +0430

    chore:Setup miro dependencies

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3001
 
-// app.use('/', express.static(path.join(__dirname, 'dist'), { index: "app.html" }))
 app.use('/', express.static(path.join(__dirname, 'dist')))
+// app.all('*', function (req, res) {
+//     res.sendFile(__dirname + 'src/app/app.html')
+// })
 
 app.listen(port, () => console.log("Listening on Port", port))
--- src/external-services.tsx
+++ src/external-services.tsx
@@ -57,7 +57,7 @@ const createMockedDependencies = (): IExternalServices => {
     } as const
 }
 
-//const ExternalServices = createMockedDependencies()
- const ExternalServices = createMockedDependencies()
+//const ExternalServices = createMiroDependencies()
+ const ExternalServices = createMiroDependencies()
 
 export { ExternalServices }
