c5dde218333e8a9e00f0bb7362b9e90d6f37ebef
Mohsen-Bazmi <Mohsen.Bazmi@gmail.com>
2021-09-08 01:49:03 +0430

    fix:(template-compile/custom helpers): map => foreach

--- src/app/template-processing/template-compiler.ts
+++ src/app/template-processing/template-compiler.ts
@@ -8,7 +8,7 @@ import Handlebars from 'handlebars/dist/cjs/handlebars';
 const customHelpers = Object.entries(stringCaseHelpers)
                     .map((h:[string, ((str: string) => string)])=>[h[0],(x:string)=>h[1](!x?'':x)])
                     .concat(Object.entries(helpers))
-customHelpers.map(([name, fn]) => Handlebars.registerHelper(name, fn))
+customHelpers.forEach(([name, fn]) => Handlebars.registerHelper(name, fn))
 
 // Object.entries(stringCaseHelpers).map(([name, fn]) => Handlebars.registerHelper(name, fn))
 // Object.entries(helpers).map(([name, fn]) => Handlebars.registerHelper(name, fn))
