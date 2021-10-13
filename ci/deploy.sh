15a3763354cc5369c73acab0cf432f35a63076da
Mohsen Bazmi <Mohsen.Bazmi@gmail.com>
2021-10-13 17:40:32 +0330

    chore(ci/deploy.sh): Refactor deployment

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,11 +10,7 @@ git add .
 git commit -m 'chore:Setup miro dependencies' 
 npm run build 
 npx gh-pages -d dist --repo https://github.com/ScenarioHunting/ScenarioHunting 
-sed -i 's|ExternalServices = createMiroDependencies|ExternalServices = createMockedDependencies|g' ./src/external-services.tsx 
-git add . 
-git commit -m 'chore: setup development dependencies' 
 git checkout deployment
-git merge deployment
-git branch -d deployment
+git branch -D deployment
 
 cd -
