67750b7aec11c8704ae12107600c2a85fb6a5d57
Mohsen-Bazmi <mohsen.bazmi@gmail.com>
2023-04-09 14:32:42 +0330

    chore:Setup miro dependencies

--- src/external-services.tsx
+++ src/external-services.tsx
@@ -56,7 +56,7 @@ const createMockedDependencies = (): IExternalServices => {
 }
 
 
-const ExternalServices = createMockedDependencies()
+const ExternalServices = createMiroDependencies()
 
 
 export { ExternalServices }
